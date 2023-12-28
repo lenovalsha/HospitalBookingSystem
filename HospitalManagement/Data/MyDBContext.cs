ef56634339119dcfe4c55f4c62db4c2f2f765337
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2023-12-27 18:27:12 -0600

    Added the connectionstring, DbContext, DbSets

---
+++ HospitalManagement/Data/MyDBContext.cs
@@ -0,0 +1,30 @@
+﻿using HospitalManagement.Models;
+using Microsoft.EntityFrameworkCore;
+namespace HospitalManagement.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<AppointmentType> AppointmentTypes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserLevel> UserLevels { get; set; }
+
+        //this is for many to many table - I dont believe we have any
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
+
+    }
+}
