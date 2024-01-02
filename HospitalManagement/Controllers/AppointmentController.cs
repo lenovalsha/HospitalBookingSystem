7c688476e2b973cf78d05dfefe59afcea4fc71b6
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2024-01-01 19:14:05 -0600

    Added Appointment Controller GET

---
+++ HospitalManagement/Controllers/AppointmentController.cs
@@ -0,0 +1,29 @@
+﻿using HospitalManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using HospitalManagement.Interfaces;
+
+
+namespace HospitalManagement.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+    public class AppointmentController :Controller
+    {
+        private readonly IAppointment _appointment;
+
+        public AppointmentController(IAppointment appointment)
+        {
+            _appointment = appointment;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+        public IActionResult GetAppointments()
+        {
+            var apt = _appointment.GetAppointments();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(apt);
+        }
+
+    }
+}
---
+++ HospitalManagement/Controllers/UserLevelController.cs
@@ -0,0 +1,6 @@
+﻿namespace HospitalManagement.Controllers
+{
+    public class UserLevelController
+    {
+    }
+}
